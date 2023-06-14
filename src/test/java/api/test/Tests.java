2379a9727ca866c9d5b6b9f01293a5e2b36ab8bd
Nathan <nathanbenattisilva@outlook.com>
2023-06-13 21:32:30 -0300

    Removing setup class to avoid resetting testSummary variable. Setting faker and random inside methods that are required

--- src/test/java/api/test/Tests.java
+++ src/test/java/api/test/Tests.java
@@ -1,13 +1,13 @@
 package api.test;
+
 import api.endpoints.EndPoints;
 import api.payload.WeatherForecast;
 import com.github.javafaker.Faker;
-import com.github.javafaker.service.RandomService;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.joda.time.DateTime;
 
 import java.util.Date;
 import java.util.Random;
@@ -15,51 +15,61 @@ import java.util.Random;
 public class Tests {
 
     Faker faker;
-    WeatherForecast weatherPayload;
     Random random;
+    private String testSummary;
 
-    @BeforeClass
-    public void setupData(){
+    @Test(priority = 1)
+    public void testPostWeather() {
+        WeatherForecast weatherPayload = new WeatherForecast();
         faker = new Faker();
         random = new Random();
-        weatherPayload = new WeatherForecast();
         weatherPayload.setSummary(faker.toString());
-        weatherPayload.setTemperatureC(random.nextInt((45 - -20) + 1) -20);
-//        weatherPayload.setDate(new Date());
-    }
-    @Test(priority = 1)
-    public void testPostWeather(){
+        weatherPayload.setTemperatureC(random.nextInt((45 - -20) + 1) - 20);
 
         Response response = EndPoints.createWeather(weatherPayload.getSummary(), weatherPayload.getTemperatureC());
         response.then().log().all();
 
         System.out.println("   ");
-        System.out.println("Summary: "+weatherPayload.getSummary());
-        System.out.println("TemperaturaC: "+weatherPayload.getTemperatureC());
-
-        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println("Summary: " + weatherPayload.getSummary());
+        testSummary = weatherPayload.getSummary();
+        System.out.println(testSummary);
+        System.out.println("TemperatureC: " + weatherPayload.getTemperatureC());
     }
-    @Test(priority = 2)
-    public void testGetWeather()
-    {
+
+    @Test(priority = 2, dependsOnMethods = "testPostWeather")
+    public void testGetWeather() {
+        WeatherForecast weatherPayload = new WeatherForecast();
+        weatherPayload.setSummary(testSummary);
+
+        System.out.println("testSummary: " + testSummary);
         Response response = EndPoints.getWeather(weatherPayload);
         response.then().log().all();
-        Assert.assertEquals(response.getStatusCode(),200);
+
+        Assert.assertEquals(response.getStatusCode(), 200);
     }
 
-    @Test(priority = 3)
-    public void testUpdateWeather()
-    {
-        Response response = EndPoints.updateWeather(weatherPayload.getSummary(),weatherPayload.getTemperatureC(),new Date(),"string");
+    @Test(priority = 3, dependsOnMethods = "testPostWeather")
+    public void testUpdateWeather() {
+        WeatherForecast weatherPayload = new WeatherForecast();
+        faker = new Faker();
+        random = new Random();
+        weatherPayload.setSummary(faker.toString());
+        weatherPayload.setTemperatureC(random.nextInt((45 - -20) + 1) - 20);
+
+        Response response = EndPoints.updateWeather(weatherPayload.getSummary(), weatherPayload.getTemperatureC(),new Date(),testSummary);
         response.then().log().all();
-        Assert.assertEquals(response.getStatusCode(),200);
-    }
-    @Test(priority = 4)
-    public void testRemoveWeather()
-    {
-        Response response = EndPoints.removeWeather(weatherPayload.getSummary(), weatherPayload);
-        response.then().log().all();
-        Assert.assertEquals(response.getStatusCode(),200);
+
+        testSummary = weatherPayload.getSummary();
+        Assert.assertEquals(response.getStatusCode(), 200);
     }
 
+    @Test(priority = 4, dependsOnMethods = "testPostWeather")
+    public void testRemoveWeather() {
+        WeatherForecast weatherPayload = new WeatherForecast();
+        weatherPayload.setSummary(testSummary);
+
+        Response response = EndPoints.removeWeather(testSummary, weatherPayload);
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
 }
